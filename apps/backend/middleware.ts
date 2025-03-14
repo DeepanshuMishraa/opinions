21c56364e99aff6aac1600d3e0ba789d618d08a4
Deepanshu <d4deepanshu723@gmail.com>
2025-03-14 22:31:44 +0530

    pull

--- apps/backend/middleware.ts
+++ apps/backend/middleware.ts
@@ -1,6 +1,7 @@
 import type { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken"
 
+
 declare global {
   namespace Express {
     interface Request {
--- apps/backend/types.d.ts
+++ apps/backend/types.d.ts
@@ -1,18 +1,7 @@
 import { Request as ExpressRequest, Response as ExpressResponse } from 'express';
 
-declare global {
-  namespace Express {
-    interface Request extends ExpressRequest {
-      email: string;
-    }
-
-    interface Response extends ExpressResponse { }
-  }
-}
-
-// Fix for Express route handler return type issue
-declare module 'express-serve-static-core' {
-  interface RequestHandler {
-    (req: Express.Request, res: Express.Response, next: NextFunction): any;
+declare namespace Express {
+  interface Request {
+    email: string;
   }
 }
